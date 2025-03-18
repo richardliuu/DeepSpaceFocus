160a3ba04a9e8e07bebc2f0d5627cff5c04f63d3
richardliuu <richardliu200127@gmail.com>
2025-03-17 23:41:30 -0400

    tkinter menu

--- window.py
+++ window.py
@@ -9,6 +9,22 @@ r.title("Monitoring Concentration") # Replace with an application name
 w = Label(r, text = "Welcome to DeepSpaceFocus") 
 w.pack()
 
+
+# Break Timer
+
+menu = Menu(r)
+r.config(menu=menu)
+break_timer_menu = Menu(menu)
+menu.add_cascade(label= "Break Timer", menu=break_timer_menu)
+break_timer_menu.add_command(label="Set Timer")
+break_timer_menu.add_separator()
+break_timer_menu.add_command(label="Exit", command=r.quit)
+helpmenu = Menu(menu)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="Controls")
+helpmenu.add_command(label="About")
+Label(r, text = "Time")
+
 button = tk.Button(r, text ="Stop", width = 20, command = r.destroy)
 button.pack()
 
